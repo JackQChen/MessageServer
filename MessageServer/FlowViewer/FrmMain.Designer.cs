e8f73f9bf37e6bc5c083d587b74d8a6aa33199db
JackChen <121955443@qq.com>
2018-08-30 14:29:01 +0800

    add statistics chart

---
+++ MessageServer/FlowViewer/FrmMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace FlowViewer
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wbPerformance = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // wbPerformance
+            // 
+            this.wbPerformance.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbPerformance.Location = new System.Drawing.Point(0, 0);
+            this.wbPerformance.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbPerformance.Name = "wbPerformance";
+            this.wbPerformance.ScriptErrorsSuppressed = true;
+            this.wbPerformance.Size = new System.Drawing.Size(284, 262);
+            this.wbPerformance.TabIndex = 0;
+            this.wbPerformance.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wbPerformance_DocumentCompleted);
+            this.wbPerformance.SizeChanged += new System.EventHandler(this.wbPerformance_SizeChanged);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.wbPerformance);
+            this.Name = "FrmMain";
+            this.Text = "流量查看器";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wbPerformance;
+    }
+}
+
---
+++ MessageServer/FlowViewer/FrmMain.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlowViewer
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        Process serverProc;
+
+        private void FrmMain_Load(object sender, System.EventArgs e)
+        {
+            this.wbPerformance.Navigate(AppDomain.CurrentDomain.BaseDirectory + "index.html");
+            Task.Factory.StartNew(() =>
+            {
+                int count = 0;
+                while (true)
+                {
+                    if (serverProc != null)
+                        if (serverProc.HasExited)
+                            Application.Exit();
+                    Thread.Sleep(10000);
+                    count++;
+                    if (count > 5)
+                    {
+                        this.ClearMemory();
+                        count = 0;
+                    }
+                }
+            });
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "SetProcessWorkingSetSize")]
+        public static extern int SetProcessWorkingSetSize(IntPtr process, int minSize, int maxSize);
+
+        public void ClearMemory()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            SetProcessWorkingSetSize(Process.GetCurrentProcess().Handle, -1, -1);
+        }
+
+        private void wbPerformance_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            this.wbPerformance.Document.InvokeScript("initRate", new object[] { this.wbPerformance.Height });
+        }
+
+        private void wbPerformance_SizeChanged(object sender, System.EventArgs e)
+        {
+            if (this.wbPerformance.Document != null)
+                this.wbPerformance.Document.InvokeScript("initRate", new object[] { this.wbPerformance.Height });
+        }
+
+        protected override void DefWndProc(ref Message m)
+        {
+            if (m.Msg == 0x0C)
+            {
+                var strText = Marshal.PtrToStringUni(m.LParam);
+                switch ((int)m.WParam)
+                {
+                    case 0:
+                        {
+                            if (this.wbPerformance.Document != null)
+                            {
+                                var rate = strText.Split(',');
+                                this.wbPerformance.Document.InvokeScript("setRate", new object[] { Convert.ToInt32(rate[0]), Convert.ToInt32(rate[1]) });
+                            }
+                        }
+                        break;
+                    case 1:
+                        {
+                            this.serverProc = Process.GetProcessById(Convert.ToInt32(strText));
+                        }
+                        break;
+                }
+            }
+            base.DefWndProc(ref m);
+        }
+    }
+}
